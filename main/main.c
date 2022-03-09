1c41732cfdad99c82974a953edc48069f852b591
Chris Johnson <tnifc@protonmail.com>
2022-03-09 14:51:55 -0500

    License

--- main/main.c
+++ main/main.c
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Chris Johnson <tinfc@protonmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
