5fb7c10e67d6df8e344071b3feae7b84a3e38545
Chris Johnson <tnifc@protonmail.com>
2024-10-20 14:38:20 -0400

    Add A2DP sink

--- main/main.cpp
+++ main/main.cpp
@@ -2,13 +2,21 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "BluetoothA2DPSinkQueued.h"
+
+I2SStream out;
+BluetoothA2DPSinkQueued a2dp_sink(out);
+
 
 extern "C" void app_main(void)
 {
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+    I2SConfig i2s_config;
+    i2s_config.pin_ws = 12;
+    i2s_config.pin_bck = 13;
+    i2s_config.pin_data = 15;
+    out.begin(i2s_config);
+    // out.end();
+
+    a2dp_sink.set_task_core(0);
+    a2dp_sink.start("ESP_SPEAKER");
 }
